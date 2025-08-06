8c212ec95e33da0852850bcfb0ee84f08b7eaff1
Weeredjs <jstirling843@gmail.com>
2025-08-06 12:59:30 -0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,23 @@
-console.log("App component loaded!");
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./ui/App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "react-auth-kit";
 import "./index.css";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <AuthProvider
+      authType="cookie"
+      authName="_auth"
+      cookieDomain={window.location.hostname}
+      cookieSecure={window.location.protocol === "https:"}
+    >
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
